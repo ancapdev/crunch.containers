a193883ec2258702cd32aeb142648915c01f0f68
Christian Rorvik <christian.rorvik@gmail.com>
2011-11-13 22:08:41 +0000

    Renamed dependencies crunch_test and crunch_base to crunch.test and crunch.base

--- configure.cmake
+++ configure.cmake
@@ -1,6 +1,6 @@
 # Copyright (c) 2011, Christian Rorvik
 # Distributed under the Simplified BSD License (See accompanying file LICENSE.txt)
 
-vpm_depend(crunch_base)
+vpm_depend(crunch.base)
 
 vpm_include_directories(${CMAKE_CURRENT_LIST_DIR}/include)
