0e554bb6faac0b03d1548b3f0e3bfb4f273e939f
Christian Rorvik <christian.rorvik@gmail.com>
2011-11-26 00:48:52 +0000

    Set default versions for dependencies

--- configure.cmake
+++ configure.cmake
@@ -1,6 +1,7 @@
 # Copyright (c) 2011, Christian Rorvik
 # Distributed under the Simplified BSD License (See accompanying file LICENSE.txt)
 
+vpm_set_default_version(crunch.base trunk)
 vpm_depend(crunch.base)
 
 vpm_include_directories(${CMAKE_CURRENT_LIST_DIR}/include)
