4cdf3b1e43c2eca54d0310afff9a5c23ffb277a3
Christian Rorvik <christian.rorvik@gmail.com>
2013-11-09 19:01:33 +0000

    Set crunch.test and crunch.base default versions to master

--- configure.cmake
+++ configure.cmake
@@ -1,7 +1,7 @@
-# Copyright (c) 2011, Christian Rorvik
+# Copyright (c) 2011-2013, Christian Rorvik
 # Distributed under the Simplified BSD License (See accompanying file LICENSE.txt)
 
-vpm_set_default_version(crunch.base trunk)
+vpm_set_default_version(crunch.base master)
 vpm_depend(crunch.base)
 
 vpm_include_directories(${CMAKE_CURRENT_LIST_DIR}/include)
